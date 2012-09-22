d4d96207698993de07625193fcd741ca65189f68
Michael Whalen <michael@whalesalad.com>
2012-09-22 14:31:12 -0400

    Cleaning up the models a bit. Moved logic for finding nearby locations into the model

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,3 @@
-require 'rest_client'
-require 'json'
-
 class LocationsController < ApplicationController
   skip_before_filter :require_token_auth, :only => [:index, :show, :search]
   before_filter :get_location, :only => [:show]
@@ -15,40 +12,10 @@ class LocationsController < ApplicationController
 
   def search
     @locations = []
+    
     if params[:latitude] and params[:longitude]
       # we're doing a geo query!!!
-      
-      # Using Geonames...
-      geonames_data = RestClient.get 'http://api.geonames.org/findNearbyPlaceNameJSON', { :params => {
-        :lat => params[:latitude],
-        :lng => params[:longitude],
-        :radius => 60,
-        :maxRows => 200,
-        :username => 'whalesalad'
-      }}
-
-      parsed = JSON.parse(geonames_data.body)['geonames']
-
-      parsed.each do |location|
-        if location['population'] > 10
-          _loc_name = "#{location['toponymName']}, #{location['adminName1']}"
-
-          _loc = Location.find_or_create_by_name(:name => _loc_name,
-                                                 :country => location['countryCode'], 
-                                                 :admin_name => location['adminName1'],
-                                                 :admin_code => location['adminCode1'],
-                                                 :locality => location['toponymName'],
-                                                 :latitude => location['lat'],
-                                                 :longitude => location['lng'])
-          
-          # Store distance. We should be doing this ourselves but whateva
-          _loc[:distance] = location['distance']
-
-          @locations.push _loc
-        end
-      end
-
-      @locations.sort_by! { |l| l[:distance].to_i }
+      @locations = Location.find_near_point(params[:latitude], params[:longitude])
     end
 
     respond_with @locations
@@ -58,7 +25,6 @@ class LocationsController < ApplicationController
   def show
     respond_with @location
   end
-
   
   def create
     @location = Location.new(params[:location])
@@ -69,7 +35,7 @@ class LocationsController < ApplicationController
       respond_with(@location, status: :unprocessable_entity)
     end
   end
-
+  
   protected
 
   def get_location
--- app/models/location.rb
+++ app/models/location.rb
@@ -14,9 +14,63 @@
 #  country             :string(255)
 #
 
+require 'rest_client'
+require 'json'
+
 class Location < ActiveRecord::Base
+  attr_accessor :distance
+
   attr_accessible :name, :latitude, :longitude, :facebook_id, 
     :locality, :admin_name, :admin_code, :country
+
+  # Class Methods
+  class << self
+    def raw_geonames_places_near(latitude, longitude)
+      geonames_url = "http://api.geonames.org/findNearbyPlaceNameJSON"
+
+      geonames_data = RestClient.get geonames_url, { :params => {
+        :lat => latitude,
+        :lng => longitude,
+        :radius => 60,
+        :maxRows => 200,
+        :username => 'whalesalad'
+      }}
+
+      JSON.parse(geonames_data.body)['geonames']
+    end
+
+    def find_near_point(latitude, longitude)
+      results = Array.new
+
+      raw_locations = raw_geonames_places_near latitude, longitude
+
+      raw_locations.each do |raw_location|
+        if raw_location['population'] > 10
+          location_name = "#{raw_location['toponymName']}, #{raw_location['adminName1']}"
+
+          location = Location.find_or_create_by_name(:name => location_name,
+                                                     :country => raw_location['countryCode'], 
+                                                     :admin_name => raw_location['adminName1'],
+                                                     :admin_code => raw_location['adminCode1'],
+                                                     :locality => raw_location['toponymName'],
+                                                     :latitude => raw_location['lat'],
+                                                     :longitude => raw_location['lng'])
+          
+          # Temporarily store distance for this result set. 
+          # We should be doing this ourselves but whateva
+          location.distance = raw_location['distance']
+
+          results.push location
+        end
+      end
+
+      results.sort_by! { |l| l.distance.to_i }
+
+      return results
+    end
+  end
+  # End class methods
+  # 2 indent ruby is dizzying. end end end end end.
 
   def as_json(options={})
     exclude = [:created_at, :updated_at]
@@ -32,4 +86,5 @@ class Location < ActiveRecord::Base
     
     result
   end
+
 end
