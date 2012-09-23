10b06a34f795cd4ff55e8fe28a222648fcf86e45
Michael Whalen <michael@whalesalad.com>
2012-09-23 01:36:38 -0400

    Added force_ssl to admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,8 @@ require 'date'
 
 class AdminController < ApplicationController
   skip_before_filter :require_token_auth
+  
+  before_filter :force_ssl if Rails.env.production?
   before_filter :authenticate if Rails.env.production?
   
   layout 'admin'
@@ -15,6 +17,12 @@ class AdminController < ApplicationController
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
       username == 'admin' && password == "Go Belluba!"
+    end
+  end
+
+  def force_ssl
+    unless request.ssl?
+      redirect_to :protocol => 'https'
     end
   end
 end
