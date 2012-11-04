37509a50755097498cc9d35c9f70017f1f0e0ae6
Michael Whalen <michael@whalesalad.com>
2012-11-04 17:54:26 -0500

    New sql annotations for location

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,18 +2,22 @@
 #
 # Table name: locations
 #
-#  id          :integer         not null, primary key
-#  name        :string(255)     not null
-#  locality    :string(255)
-#  admin_name  :string(255)
-#  admin_code  :string(255)
-#  country     :string(255)
-#  latitude    :float
-#  longitude   :float
-#  facebook_id :integer(8)
-#  created_at  :datetime        not null
-#  updated_at  :datetime        not null
-#  users_count :integer         default(0)
+#  id               :integer         not null, primary key
+#  name             :string(255)     not null
+#  locality         :string(255)
+#  state            :string(255)
+#  country          :string(255)
+#  latitude         :float
+#  longitude        :float
+#  facebook_id      :integer(8)
+#  created_at       :datetime        not null
+#  updated_at       :datetime        not null
+#  foursquare_id    :string(255)
+#  venue            :string(255)
+#  users_count      :integer         default(0)
+#  address          :string(255)
+#  geoname_id       :integer
+#  activities_count :integer         default(0)
 #
 
 class Location < ActiveRecord::Base
