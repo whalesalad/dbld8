dca73564c9c01ac0529dbaa3077c17ecc98c97c3
Michael Whalen <michael@whalesalad.com>
2012-11-08 20:59:15 -0500

    Adding asset sync ability.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,13 +4,12 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     :provider               => "AWS",
-    :aws_access_key_id      => ENV["S3_KEY"],
-    :aws_secret_access_key  => ENV["S3_SECRET"],
+    :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],
+    :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],
   }
 
-  config.fog_directory  = "static.dbld8.com"
-  # config.fog_host       = "http://static.dbld8.com"
-  config.asset_host     = "http://static.dbld8.com"
+  config.fog_directory  = ENV["FOG_DIRECTORY"]
+  config.asset_host     = "http://#{ENV["FOG_DIRECTORY"]}"
   config.fog_public     = true
   config.fog_attributes = { 'Cache-Control' => 'max-age=315576000' }
 end
