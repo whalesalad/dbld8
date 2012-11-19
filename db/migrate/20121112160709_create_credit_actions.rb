adc56308a62adf06bd01fb82c1ee555167e3ddfe
Michael Whalen <michael@whalesalad.com>
2012-11-18 22:00:10 -0500

    Added some initial stuff to the migration for creedit actions.

--- db/migrate/20121112160709_create_credit_actions.rb
+++ db/migrate/20121112160709_create_credit_actions.rb
@@ -8,5 +8,14 @@ class CreateCreditActions < ActiveRecord::Migration
     end
 
     add_index :credit_actions, :slug, :unique => true
+
+    # Create some initial credit actions.
+    credit_actions = {
+      'registration' => 1000,
+      'activity_create' => -10
+    }.each do |slug, cost|
+      CreditAction.create(:slug => slug, :cost => cost)
+    end
+
   end
 end
