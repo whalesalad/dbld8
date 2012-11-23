9250b539d439d1642debec91026ecd915796b36f
Michael Whalen <michael@whalesalad.com>
2012-11-23 15:09:46 -0800

    Added stdout sync to prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 DoubleDate::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  # Temporarily for foreman dbug
+  STDOUT.sync = true
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
