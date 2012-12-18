beb1cb616b85207b7ff2c4920b914dece0a3993b
Michael Whalen <michael@whalesalad.com>
2012-12-18 04:55:32 +0000

    Removed 20121218035656_remove_active_engagement_id.rb

--- db/migrate/20121218035656_remove_active_engagement_id.rb
+++
@@ -1,5 +0,0 @@
-class RemoveActiveEngagementId < ActiveRecord::Migration
-  def change
-    remove_column :activities, :active_engagement_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121218035656) do
+ActiveRecord::Schema.define(:version => 20121213131237) do
 
   create_table "activities", :force => true do |t|
     t.string   "title",       :null => false
