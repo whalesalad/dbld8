112417398455d134494dfe44b223e8027c0d5f36
Michael Whalen <michael@whalesalad.com>
2012-12-18 04:54:33 +0000

    Slight model annotations tweaks.

--- test/unit/activity_test.rb
+++ test/unit/activity_test.rb
@@ -2,18 +2,17 @@
 #
 # Table name: activities
 #
-#  id                   :integer         not null, primary key
-#  title                :string(255)     not null
-#  details              :string(255)
-#  day_pref             :string(255)
-#  time_pref            :string(255)
-#  location_id          :integer
-#  user_id              :integer         not null
-#  wing_id              :integer         not null
-#  status               :string(255)
-#  created_at           :datetime        not null
-#  updated_at           :datetime        not null
-#  active_engagement_id :integer
+#  id          :integer         not null, primary key
+#  title       :string(255)     not null
+#  details     :string(255)
+#  day_pref    :string(255)
+#  time_pref   :string(255)
+#  location_id :integer
+#  user_id     :integer         not null
+#  wing_id     :integer         not null
+#  status      :string(255)
+#  created_at  :datetime        not null
+#  updated_at  :datetime        not null
 #
 
 require 'test_helper'
