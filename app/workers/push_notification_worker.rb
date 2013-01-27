5f1a9e2e91aeddaaab37a7c2f59b27f1f7e63a87
Michael Whalen <michael@whalesalad.com>
2013-01-26 21:09:20 -0500

    Updated PEM files and such.

--- app/workers/push_notification_worker.rb
+++ app/workers/push_notification_worker.rb
@@ -4,17 +4,18 @@ class PushNotificationWorker
   attr_accessor :pusher
 
   def pusher
-    @pusher ||= Grocer.pusher(certificate: Rails.root.join("cert/apn_dev.pem"))
+    @pusher ||= Grocer.pusher(certificate: Rails.root.join("cert/apn_#{Rails.env}.pem"))
   end
 
   def perform(n_id)
     notification = Notification.find_by_id(n_id)
 
     if notification && notification.pushable?
+      
       push_notification = Grocer::Notification.new(
         device_token: notification.user.primary_device_token,
         alert:        notification.to_s,
-        badge:        42,
+        # badge:        42,
         # sound:        "siren.aiff",         # optional
         # expiry:       Time.now + 60*60,     # optional; 0 is default, meaning the message is not stored
         # identifier:   1234                  # optional
