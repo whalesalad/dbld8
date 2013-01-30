660df925a313363299b9b41aba25b530f76ac901
Michael Whalen <michael@whalesalad.com>
2013-01-29 20:12:12 -0500

    Adding an error callback to segmentio stuff.

--- config/initializers/segmentio.rb
+++ config/initializers/segmentio.rb
@@ -1,1 +1,4 @@
-Analytics.init(secret: 'fi31zj6ejhs4amywwgsh')
+Analytics.init(
+  secret: 'fi31zj6ejhs4amywwgsh', 
+  on_error: Proc.new {|s,m| puts "[ANALYTICS] Error! Status: #{s}, Message: #{m}"}
+)
