c246d33113790000764efdc32e826d651a7295e7
Michael Whalen <michael@whalesalad.com>
2013-02-08 14:43:48 +0100

    Unicorn gives me nothing but grief.

--- config/initializers/segmentio.rb
+++ config/initializers/segmentio.rb
@@ -1,6 +1,6 @@
-if Rails.env.development?
-  Analytics.init(
-    secret: Rails.configuration.segment_io_secret,
-    on_error: Rails.configuration.segment_io_error
-  )
-end
+# if Rails.env.development?
+Analytics.init(
+  secret: Rails.configuration.segment_io_secret,
+  on_error: Rails.configuration.segment_io_error
+)
+# end
