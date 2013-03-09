63eeddc1b78bdef15a46f8100d1abe7839c54a6b
Michael Whalen <michael@whalesalad.com>
2013-03-10 00:27:43 +0100

    Adjusted cron tasks to run every 4 hours instead of hourly

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,13 @@
 set :output, "#{path}/log/cron.log"
 
-every :hour, :roles => :cron do
+# every :hour, :roles => :cron do
+  
+# end
+
+every 4.hours, :roles => :cron do
   # run apn feedback worker
   runner "PushFeedbackWorker.perform_async"
-
+  
   # Run a database backup
   rake "pg:backup"
 end
