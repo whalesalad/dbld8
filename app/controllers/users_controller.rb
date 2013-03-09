3766a8cb492529254ea2bb943cec97d33b8b91d1
Michael Whalen <michael@whalesalad.com>
2013-03-09 21:23:36 +0100

    Fixed login error.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,8 @@ class UsersController < ApplicationController
 
       @user.accessible = [:facebook_access_token]
       @user.facebook_access_token = params[:facebook_access_token]
+
+      @user.create_token
       
       if @user.save!
         render json: @user.token.as_json.merge(new_user: @new_user) and return
