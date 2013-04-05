400e228a18ae9be63b80bca7d3d01e2ed8215590
Michael Whalen <michael@whalesalad.com>
2013-04-05 16:07:25 +0200

    Reduced unicorn workers on staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -9,6 +9,8 @@ set_default(:postgresql_user) { application }
 set_default(:postgresql_password) { "never fly without your wings" }
 set_default(:postgresql_database) { "doubledate_staging" }
 
+set :unicorn_workers, 2
+
 set :default_environment, {
   'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
 }
