be70b33f6e007c1aad7278b4d31cfe3974e43070
Michael Whalen <michael@whalesalad.com>
2013-04-05 13:13:23 +0200

    Adding staging mods

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,8 +2,13 @@
 server "198.199.124.246", :web, :app, :search, :db, :primary => true
 
 set :branch, "staging"
+set :rails_env, "staging"
 
 set_default(:postgresql_host, 'localhost')
 set_default(:postgresql_user) { application }
 set_default(:postgresql_password) { "never fly without your wings" }
 set_default(:postgresql_database) { "doubledate_staging" }
+
+set :default_environment, {
+  'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
+}
