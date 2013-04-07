9ee245026db0c98b9358a5da752914f80c5b43ca
Michael Whalen <michael@whalesalad.com>
2013-04-08 00:42:55 +0200

    Adjustments to Librato Metrics

--- config/initializers/librato.rb
+++ config/initializers/librato.rb
@@ -1,5 +1,7 @@
 if Rails.env.production?
   require 'metriks'
   require 'metriks/reporter/librato_metrics'
-  Metriks::Reporter::LibratoMetrics.new(Rails.configuration.librato['user'], Rails.configuration.librato['token']).start
+  
+  reporter = Metriks::Reporter::LibratoMetrics.new(Rails.configuration.librato['user'], Rails.configuration.librato['token'], on_error: proc { |e| Rails.logger.info("LibratoMetrics: #{ e.message }") })
+  reporter.start
 end
